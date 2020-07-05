2c4497a6f80a1a2d660f148db49231f6ddfcf01b
Nwankwo Samuel <nwankwosami@gmail.com>
2020-07-05 02:23:40 +0100

    create utli class for base api url connection

---
+++ app/src/main/java/com/samuel/books/apiUtlis.java
@@ -0,0 +1,41 @@
+package com.samuel.books;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class apiUtlis {
+	public static final String BASE_API_URL =
+			"https://www.googleapis.com/books/v1/volumes";
+	
+	private apiUtlis() {
+	}
+	
+	public static URL buildUrl(String title) {
+		
+		String fullUrl = BASE_API_URL + "q" + title;
+		URL url = null;
+		
+		try {
+			
+			url = new URL(fullUrl);
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return url;
+	}
+	
+	public static String getJson(URL url) throws IOException {
+		
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		InputStream stream = connection.getInputStream();
+		Scanner scanner = new Scanner(stream);
+		scanner.useDelimiter("\\A");
+		
+	}
+}
+
