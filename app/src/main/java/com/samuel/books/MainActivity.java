9e1ece6da5fffc26c1cf4a4533847535832850de
Nwankwo Samuel <nwankwosami@gmail.com>
2020-07-05 01:38:56 +0100

    Initial commit

---
+++ app/src/main/java/com/samuel/books/MainActivity.java
@@ -0,0 +1,14 @@
+package com.samuel.books;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bookList_activity);
+	}
+}
